619f5533b15c22a9d07955fbe2cc44f364ea5c16
Jenee Geer <jenee@plie.codes>
2019-06-24 16:59:06 -0500

    add gitignore and setup files for testing

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rspec-expense-tracker
+
+# run app with command
+* curl localhost:9292/expenses/2017-06-17 -w "\n"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,8 @@ RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+  config.filter_gems_from_backtrace 'rack', 'rack-test', 'sequel', 'sinatra'
+  # if need full backtrace, pass --backtrace or -b flag to RSpec
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
@@ -69,7 +71,7 @@ RSpec.configure do |config|
 
   # This setting enables warnings. It's recommended, but in some cases may
   # be too noisy due to issues in dependencies.
-  config.warnings = true
+  # config.warnings = true
 
   # Many RSpec users commonly either run the entire suite or an individual
   # file, and it's useful to allow more verbose output when running an
@@ -84,7 +86,7 @@ RSpec.configure do |config|
   # Print the 10 slowest examples and example groups at the
   # end of the spec run, to help surface which specs are running
   # particularly slow.
-  config.profile_examples = 10
+  # config.profile_examples = 10
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
